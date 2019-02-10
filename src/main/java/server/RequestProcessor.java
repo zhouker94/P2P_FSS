a1cf9ba66862136b58c0b6d42a7e638e69f34f5d
OwenZhu <398094982@qq.com>
2019-02-10 21:28:45 +0800

    update host information

--- src/main/java/server/RequestProcessor.java
+++ src/main/java/server/RequestProcessor.java
@@ -4,8 +4,7 @@ import org.json.simple.JSONObject;
 
 public interface RequestProcessor {
 	
-    @SuppressWarnings("serial")
-    public static class RequestProcessorException extends Exception {
+    class RequestProcessorException extends Exception {
         public RequestProcessorException(String msg, Throwable t) {
             super(msg, t);
         }
